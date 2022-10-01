02431971f6f63b202aae5b527b6927ec9f4d154d
Stracey2 <94061653+Mstracey2@users.noreply.github.com>
2022-10-01 15:41:15 +0100

    Added my parts

--- Assets/Scripts/ragdoll.cs
+++ Assets/Scripts/ragdoll.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ragdoll : MonoBehaviour
 {
-    public ConfigurableJoint ragdollJoint;
+    //array for all the joints that is used to make the player ragdoll
+    public ConfigurableJoint[] ragdollJoints = new ConfigurableJoint[] {};
+
     public JointDrive joint;
-    [SerializeField]private bool isGrounded = true;
+
+    //bool for determining whether player is on the ground
+    private bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,16 +21,43 @@ public class ragdoll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         if (isGrounded == false)
         {
-            joint = ragdollJoint.angularXDrive;
-            joint.positionSpring = 0f;
-            ragdollJoint.angularXDrive = joint;
+            foreach (ConfigurableJoint Rjoint in ragdollJoints)
+            {
+                //if the player is off the ground, every limb in the array will ragdoll
+                joint = Rjoint.angularXDrive;
+                joint.positionSpring = 0f;
+                Rjoint.angularXDrive = joint;
+            }
         }
+        else
+        {
+            foreach (ConfigurableJoint Rjoint in ragdollJoints)
+            {
+                //if the player is grounded, limbs return to normal
+                joint = Rjoint.angularXDrive;
+                if(Rjoint.name == "Hips")
+                {
+                    joint.positionSpring = 750f;
+                }
+                else
+                {
+                    joint.positionSpring = 180f;
+                }
+                
+                Rjoint.angularXDrive = joint;
+            }
+        }
+
+        
     }
 
     void OnCollisionEnter(Collision collisioninfo)
     {
+        //collision with ground
         if (collisioninfo.gameObject.tag =="Ground")
         {
             isGrounded = true;
@@ -36,6 +67,7 @@ public class ragdoll : MonoBehaviour
 
     private void OnCollisionExit(Collision collisioninfo)
     {
+        //off the ground
         if (collisioninfo.gameObject.tag == "Ground")
         {
             isGrounded = false;
