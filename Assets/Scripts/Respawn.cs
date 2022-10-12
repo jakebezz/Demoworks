7c9ea0e9aaea6e6ebcae24aee2fb48efbcdb420f
tofuthegreen <thegreentofu@gmail.com>
2022-10-12 20:44:09 +0100

    Added more to level, Decorated main menu, added death/respawn function

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("touched");
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 5,0 * Time.deltaTime);
+    }
+}
+
