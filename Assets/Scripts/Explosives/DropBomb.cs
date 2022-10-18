efcf04a7fac4ce670a598b2837da1b01abd79e81
Stracey2 <94061653+Mstracey2@users.noreply.github.com>
2022-10-18 02:20:58 +0100

    Added comments to landmine

--- Assets/Scripts/Explosives/DropBomb.cs
+++ Assets/Scripts/Explosives/DropBomb.cs
@@ -6,60 +6,85 @@ public class DropBomb : MonoBehaviour
 {
     [SerializeField] private GameObject landMine;
     [SerializeField] private GameObject grenade;
+
     [SerializeField] private GameObject spawnLocationLandmine;
     [SerializeField] private GameObject spawnLocationGrenade;
+   
+    //player ref
     public GameObject character;
 
-    //For Testing
-    [SerializeField] private GameObject tempGrenade;
-
-
+    //clone of either a landmine or grenade
     private GameObject clone;
 
-    private float coolDown = 0;
-    private bool pressed = false;
-    
-    [SerializeField] GameObject playerObj;
+    //cooldown for landmine
+    private float landmineCoolDown = 0;
+    private bool spacePressed = false;
 
-    private void Start()
-    {
-        
-    }
+    //cooldown for grenade
+    private float grenadeCoolDown = 0;
+    private bool bPressed = false;
+
+    [SerializeField] GameObject playerObj;
 
     void Update()
     {
-        
-       
-
-        
-        if (pressed == true)
+        //starts the cooldown for the landmine if space is pressed
+        if (spacePressed)
         {
-            coolDown -= Time.deltaTime;
-            if(coolDown <= 0)
+            landmineCoolDown -= Time.deltaTime;
+            if(landmineCoolDown <= 0)
             {
-                pressed = false;
+                spacePressed = false;
+            }
+        }
+        
+        //starts the cooldown for the grenade if b is pressed
+        if (bPressed)
+        {
+            grenadeCoolDown -= Time.deltaTime;
+            if (grenadeCoolDown <= 0)
+            {
+                bPressed = false;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && pressed == false)
+        if (Input.GetKeyDown(KeyCode.Space) && spacePressed == false)
         {
-            Vector3 playerVelocity = character.GetComponent<Rigidbody>().velocity;
-            coolDown = 3;
+            //cooldown length
+            landmineCoolDown = 3;
+
+            //creates landmine
             clone = Instantiate(landMine, new Vector3(spawnLocationLandmine.transform.position.x, spawnLocationLandmine.transform.position.y, spawnLocationLandmine.transform.position.z), new Quaternion(0, 0, 0, 0));
+           
+            // get player velocity
+            Vector3 playerVelocity = character.GetComponent<Rigidbody>().velocity;
+
+            /* this will match the landmine's velocity to the player's,
+            * 
+            * this makes it so the landmine will keep up with the player if they are running.
+            * 
+            * without this, the landmine would simply fly backwards and wont go over the player's head.
+            */
             clone.GetComponent<Rigidbody>().velocity = playerVelocity;
+
+            //adds a force to arch the landmine up and over the player
             clone.GetComponent<Rigidbody>().AddForce(transform.right * 500 + transform.up * 500);
             
-            pressed = true;
+            spacePressed = true;
         }
-        else if (Input.GetKeyDown(KeyCode.B) && pressed == false)
+
+        else if (Input.GetKeyDown(KeyCode.B) && bPressed == false)
         {
+            //creates grenade
             clone = Instantiate(grenade, new Vector3(spawnLocationGrenade.transform.position.x, spawnLocationGrenade.transform.position.y, spawnLocationGrenade.transform.position.z), new Quaternion(0, 0, 0, 0));
-            coolDown = 3;
-            pressed = true;
+            //cooldown
+            grenadeCoolDown = 3;
+            bPressed = true;
         }
-        else if(Input.GetKeyDown(KeyCode.K) && pressed == false)
-        {
-            clone = Instantiate(tempGrenade, new Vector3(spawnLocationGrenade.transform.position.x, spawnLocationGrenade.transform.position.y, spawnLocationGrenade.transform.position.z), new Quaternion(0, 0, 0, 0));
-        }
+    }
+
+    void help()
+    {
+
     }
 }
--- Assets/Scripts/Explosives/LandmineExplosion.cs
+++ Assets/Scripts/Explosives/LandmineExplosion.cs
@@ -8,6 +8,7 @@ public class LandmineExplosion : Explosives
 
     private void Update()
     {
+        // begins the countdown if the float is equal to the delay
         if(countdown == delay)
         {
             beginCountdown = true;
@@ -24,16 +25,19 @@ public class LandmineExplosion : Explosives
     {
         if(collision.gameObject.tag == "Player" && beginCountdown == false)
         {
+            // countdown begins if theres a collision with player
             countdown = delay;
         }
     }
 
     void countDown()
     {
+        // timer
         countdown -= Time.deltaTime;
 
         if (countdown <= 0 && beginCountdown)
         {
+            //explodes landmine
             Explode();
             beginCountdown = false;
         }
